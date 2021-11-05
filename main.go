14138dd952abf13ed4eb2881ccc2fc679b9fa9ee
xingly-cn <xiaoheikeji@vip.qq.com>
2021-11-06 00:29:39 +0800

    实现登陆JWT生成及校验

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/rs/xid"
 )
+
+//---------------------------------------视图映射---------------------------------------------
 
 // Homeindex
 func HomeIndex(c *gin.Context) {
@@ -19,6 +26,8 @@ func HomeIndex(c *gin.Context) {
 func AdminIndex(c *gin.Context) {
 	c.HTML(http.StatusOK, "admin.html", nil)
 }
+
+//---------------------------------------登陆接口---------------------------------------------
 
 // LoginParms
 type LoginParms struct {
@@ -31,18 +40,44 @@ func Login(c *gin.Context) {
 	var loginParms LoginParms
 	c.ShouldBind(&loginParms)
 
+	user := User{
+		Uuid:     xid.New().String(),
+		Username: loginParms.Username,
+		Password: loginParms.Password,
+		Phone:    "18895388211",
+		Status:   "1",
+		Picday:   100,
+	}
+
+	token, _ := jwtGenerateToken(&user, time.Hour*24*365)
+
+	data, expireTime, _ := JwtParseUser(token)
+
 	//Todo This to judge authority
 	c.JSON(http.StatusOK, gin.H{
-		"msg": loginParms,
+		"msg":    loginParms,
+		"token":  token,
+		"user":   data,
+		"expire": expireTime,
 	})
 }
 
+//---------------------------------------配置类---------------------------------------------
+
 // UploadConfig
 func UploadConfig() (*oss.Bucket, error) {
-	client, _ := oss.New("https://cdn.xingly.cn/", "LTAI4GK67v43NPGCeundD6wq", "LZdknJ9ZCB3MFM7CJopF7NK4LIds2Dg", oss.UseCname(true), oss.EnableCRC(true))
+	client, _ := oss.New("https://cdn.xingly.cn/", "LTAI4GK67v43NPGCeundD6wq", "ZdknJ9ZCB3MFM7CJopF7NK4LIds2Dg", oss.UseCname(true), oss.EnableCRC(true))
 	bucket, _ := client.Bucket("xingly")
 	return bucket, nil
 }
+
+// MysqlConfig
+func MysqlConfig() (*gorm.DB, error) {
+	db, err := gorm.Open("mysql", "root:XNXxnx520@(8.142.199.134:3306)/teapic?charset=utf8mb4")
+	return db, err
+}
+
+//---------------------------------------上传接口---------------------------------------------
 
 // Upload
 func Upload(c *gin.Context) {
@@ -60,9 +95,66 @@ func Upload(c *gin.Context) {
 	})
 }
 
+//---------------------------------------版权信息---------------------------------------------
+
 // Copyright
 func Copyright(c *gin.Context) {
 	c.HTML(http.StatusOK, "copyright.html", nil)
+}
+
+//---------------------------------------JWT 身份验证---------------------------------------------
+
+// JWT Secret
+var secret = "4680bbfa2dc33ab4f5d657658156a075"
+
+// User Entity
+type User struct {
+	Uuid     string
+	Username string
+	Password string
+	Phone    string
+	Status   string
+	Picday   int
+}
+
+// My Payload
+type userClaims struct {
+	jwt.StandardClaims
+	*User
+}
+
+// Generate Jwt
+func jwtGenerateToken(m *User, d time.Duration) (string, error) {
+	m.Password = ""
+	expireTime := time.Now().Add(d)
+	stdClaims := jwt.StandardClaims{
+		ExpiresAt: expireTime.Unix(),
+		IssuedAt:  time.Now().Unix(),
+		Id:        m.Uuid,
+		Issuer:    "WWW.XINGLY.CN",
+	}
+	uClaims := userClaims{
+		StandardClaims: stdClaims,
+		User:           m,
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, uClaims).SignedString([]byte(secret))
+	return token, err
+}
+
+// Parse Jwt
+func JwtParseUser(token string) (*User, int64, error) {
+	if token == "" {
+		return nil, 0, errors.New("Token is not null")
+	}
+	claims := userClaims{}
+	_, err := jwt.ParseWithClaims(token, &claims, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return claims.User, claims.ExpiresAt, err
 }
 
 func main() {
@@ -98,6 +190,5 @@ func main() {
 		// Interface
 		AdminPage.GET("login.go", Login)
 	}
-
-	r.Run(":9999")
+	r.Run(":8080")
 }
